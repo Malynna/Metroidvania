8c42af8717e5d6d0bb502f31a86c1315bbfaa362
Malynna <malinex@op.pl>
2022-04-25 00:24:56 +0200

    fixed moving gravity

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,18 +4,19 @@ export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
 export (int) var GRAVITY = 200
-export (int) var JUMP_FORCE = 128
+export (int) var JUMP_FORCE = 142
 export (int) var MAX_SLOPE_ANGLE = 45
-
-
 
 var motion = Vector2.ZERO
 var snap_vector = Vector2.ZERO
+var just_jumped = false
+
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 func _physics_process(delta):
+	just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizonatal_force(input_vector, delta)
 	apply_fraction(input_vector)
@@ -51,13 +52,14 @@ func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			just_jumped = true
 			snap_vector = Vector2.ZERO
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2;
 
 func apply_gravity(delta):
-#	if not is_on_floor():
+	if not is_on_floor():
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE)
 
@@ -72,11 +74,24 @@ func update_animations(input_vector):
 		spriteAnimator.play("Jump")
 
 func move():
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_motion = motion
+	var last_position = position
+	
 	motion = move_and_slide_with_snap(motion, snap_vector *4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	# Landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+		
+	# Just left ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0
+		position.y = last_position.y
 
-
-
-
+	# Prevent Sliding (hack)
+#	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 0:
+#		position.x = last_position.x
 
 
 
