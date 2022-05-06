4dea106d49efc4b668e444be71f7d013f9e7634d
Malynna <malinex@op.pl>
2022-05-07 00:38:08 +0200

    added wall slider

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,11 +10,18 @@ export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
 export (int) var GRAVITY = 200
+export (int) var WALL_SLIDE_SPEED = 48
+export (int) var MAX_WALL_SLIDE_SPEED = 128
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 export (int) var BULLET_SPEED = 250
 
+enum {
+	MOVE,
+	WALL_SLIDE
+}
 
+var state = MOVE
 var invincible = false setget set_invincible
 var motion = Vector2.ZERO
 var snap_vector = Vector2.ZERO
@@ -40,14 +47,30 @@ func _ready():
 
 func _physics_process(delta):
 	just_jumped = false
-	var input_vector = get_input_vector()
-	apply_horizonatal_force(input_vector, delta)
-	apply_fraction(input_vector)
-	update_snap_vector()
-	jump_check()
-	apply_gravity(delta)
-	update_animations(input_vector)
-	move()
+	
+	match state:
+		MOVE:
+			var input_vector = get_input_vector()
+			apply_horizonatal_force(input_vector, delta)
+			apply_fraction(input_vector)
+			update_snap_vector()
+			jump_check()
+			apply_gravity(delta)
+			update_animations(input_vector)
+			move()
+			wall_slide_check()
+		WALL_SLIDE:
+			spriteAnimator.play("Wall Slide")
+			
+			var wall_axis = get_wall_axis()
+			if wall_axis != 0:
+				sprite.scale.x = wall_axis
+			
+			wall_slide_jump_check(wall_axis)
+			wall_slide_drop_check(delta)
+			wall_slide_fast_slide_check(delta)
+			move()
+			wall_detach_check(wall_axis)
 	
 	if Input.is_action_pressed("fire") and fireBulletTimer.time_left == 0 :
 		fire_bullet()
@@ -124,6 +147,7 @@ func update_animations(input_vector):
 	if not is_on_floor():
 		spriteAnimator.play("Jump")
 
+
 func move():
 	var was_in_air = not is_on_floor()
 	var was_on_floor = is_on_floor()
@@ -144,14 +168,56 @@ func move():
 		coyoteJumpTimer.start()
 
 	# Prevent Sliding (hack)
-#	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 0:
-#		position.x = last_position.x
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 0:
+		position.x = last_position.x
 
+
+func wall_slide_check():
+	if not is_on_floor() and is_on_wall():
+		state = WALL_SLIDE
+		
+		double_jump = true
+
+func get_wall_axis():
+	var is_right_wall = test_move(transform, Vector2.RIGHT)
+	var is_left_wall = test_move(transform, Vector2.LEFT)
+	return int(is_left_wall) - int(is_right_wall)
+
+
+func wall_slide_jump_check(wall_axis):
+	if Input.is_action_just_pressed("ui_up"):
+		motion.x = wall_axis * MAX_SPEED
+		motion.y = -JUMP_FORCE/1.25
+		state = MOVE
+		
+		
+func wall_slide_drop_check(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		motion.x = ACCELERATION * delta
+		state = MOVE
+		
+	if Input.is_action_just_pressed("ui_left"):
+		motion.x = -ACCELERATION * delta
+		state = MOVE
+
+
+func wall_slide_fast_slide_check(delta):
+	var max_slide_speed = WALL_SLIDE_SPEED
+	if Input.is_action_just_pressed("ui_down"):
+		max_slide_speed = MAX_WALL_SLIDE_SPEED
+	motion.y = min(motion.y + GRAVITY * delta, max_slide_speed)
+
+
+func wall_detach_check(wall_axis):
+	if wall_axis == 0 or is_on_floor():
+		state = MOVE
+		
 
 func _on_Hurtbox_hit(damage):
 	if not invincible:
 		PlayerStats.health -= damage
 		blinkAnimator.play("Blink")
+
 
 func _on_died():
 	queue_free()
