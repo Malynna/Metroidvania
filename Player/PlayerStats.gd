782d94825ab6e9619a0b09302f2f2a01d3ef5218
Malynna <malinex@op.pl>
2022-05-06 22:27:18 +0200

    added CanvasLayer

--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -10,5 +10,5 @@ func set_health(value):
 	if value < health:
 		Events.emit_signal("add_screenshake", 0.5, 0.5)
 	health = clamp(value, 0, max_health)
-	if health ==0:
+	if health == 0:
 		emit_signal("player_died")
